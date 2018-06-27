7a560b8094b486f210ae5bbb7d01fbcfeb568683
Simon Baslé <sbasle@pivotal.io>
2018-06-27 15:42:32 +0200

    move boot application and remove tests

--- src/main/java/reactor/demo/webclient/boot/WebclientApplication.java
+++ src/main/java/reactor/demo/webclient/boot/WebclientApplication.java
@@ -1,4 +1,4 @@
-package reactor.demo.webclient;
+package reactor.demo.webclient.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
